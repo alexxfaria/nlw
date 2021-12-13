76badc2872cc4168f247735b86b5d6567c0062c6
alexxfaria <alexxfs@live.com>
2021-12-13 10:52:55 -0300

    Create middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserControllers";
 import { CreatePeopleController } from "./controllers/CreatePeopleControllers";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 const createUserController = new CreateUserController;
 const createPeopleController = new CreatePeopleController;
 
 router.post("/users", createUserController.handle);
-router.post("/peoples", createPeopleController.handle);
+router.post("/peoples", ensureAdmin, createPeopleController.handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import "reflect-metadata";
 import { router } from "./routes";
-import express  from "express";
+import express, { Request, Response, NextFunction }  from "express";
 import "./database";
+import "express-async-errors";
 
 const app = express();
 const port = process.env.API_PORT
 
 app.use(express.json());
 app.use(router);
+app.use((err: Error, request: Request, response:Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error:err.message,
+    });
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+})
 app.listen(port, () => {
   console.log("Server is runnig")
 });
